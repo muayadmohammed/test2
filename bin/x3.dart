9d43460a27281d5d20df46766c07157f829e7bf1
muayadmohammed <muayadmohammed@gmail.com>
2022-11-12 13:13:22 +0300

    comnit

--- bin/x3.dart
+++ bin/x3.dart
@@ -7,15 +7,15 @@ void main() {
   // int x = 90;
   // String y = 'Omer';
   print('Hello world: !');
+//sjsgeks
+  // var num = 40;
 
-  var num = 40;
-
-  if (num > 0) {
-    print("The number is positive");
-  } else if (num == 0) {
-    print("The number is zero");
-    print("The number is zero");
-  } else {
-    print("The number is negative");
-  }
+  // if (num > 0) {
+  //   print("The number is positive");
+  // } else if (num == 0) {
+  //   print("The number is zero");
+  //   print("The number is zero");
+  // } else {
+  //   print("The number is negative");
+  // }
 }
