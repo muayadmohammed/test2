af478d31330e9b4ee4c086f9c73d7642d92de6b4
muayadmohammed <muayadmohammed@gmail.com>
2022-11-12 13:10:33 +0300

    muayad

--- bin/x3.dart
+++ bin/x3.dart
@@ -6,13 +6,14 @@ import 'dart:io';
 void main() {
   // int x = 90;
   // String y = 'Omer';
-  // print('Hello world: !');
+  print('Hello world: !');
 
   var num = 40;
 
   if (num > 0) {
     print("The number is positive");
   } else if (num == 0) {
+    print("The number is zero");
     print("The number is zero");
   } else {
     print("The number is negative");
